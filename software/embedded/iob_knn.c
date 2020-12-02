3ebcddfa5d8ba13f8ce6e0aea3e8cedee0c8e4c1
Pedro Antunes <pedronmantunes@gmail.com>
2020-12-02 11:43:42 +0000

    tirei o done=0

--- software/embedded/iob_knn.c
+++ software/embedded/iob_knn.c
@@ -8,7 +8,6 @@ static int32_t base;
 void knn_reset(){
   IO_SET(base, KNN_RESET, 1);
   IO_SET(base, KNN_RESET, 0);
-  IO_SET(base, DONE, 0);
 }
 
 void knn_init(int32_t base_address){
@@ -30,7 +29,7 @@ void knn_get_neighbours(neighbor *v_neighbor) {
   IO_SET(base, DONE, 1);
   for(uint32_t i = 0; i<K; i++){
     IO_SET(base, SEL, i);
-    v_neighbor[i].idx=IO_GET(base, DATA_OUT);
+    v_neighbor[i].idx=(uint32_t)IO_GET(base, DATA_OUT);
   }
   knn_reset();
 }
