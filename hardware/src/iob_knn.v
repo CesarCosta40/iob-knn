de8fa1e3e0a9784fd6931f1af65aeb413313a35c
Pedro Antunes <pedronmantunes@gmail.com>
2020-12-27 19:51:57 +0000

    Added modulo

--- hardware/src/iob_knn.v
+++ hardware/src/iob_knn.v
@@ -8,7 +8,7 @@ module iob_knn
     parameter ADDR_W = `KNN_ADDR_W, //NODOC Address width
     parameter DATA_W = `DATA_W, //NODOC Data word width
     parameter WDATA_W = `KNN_WDATA_W, //NODOC Data word width on writes
-    parameter HW_K = `HW_K,  
+    parameter HW_K = `HW_K,
     parameter N_SOLVERS = `N_SOLVERS
   )
    (
@@ -28,51 +28,27 @@ module iob_knn
    `SIGNAL(write, 1)
    `COMB write = | wstrb;
 
-   //`SIGNAL(DATA_X1, 16)
-   //`SIGNAL(DATA_Y1, 16)
-   
-   reg signed [DATA_W/2-1:0] DATA_X1 [N_SOLVERS-1:0];
-   reg signed [DATA_W/2-1:0] DATA_Y1 [N_SOLVERS-1:0];
-   
-   integer j;
-
-   always @(posedge clk, posedge rst_int) begin
-     for(j=0; j < N_SOLVERS; j=j+1) begin
-      if (rst_int) DATA_X1[j] <= 0; else if (SOLVER_SEL==j) DATA_X1[j] <= DATA_1[DATA_W/2-1:0];
-    end
-   end
-
-   always @(posedge clk, posedge rst_int) begin
-     for(j=0; j < N_SOLVERS; j=j+1) begin
-      if (rst_int) DATA_Y1[j] <= 0; else if (SOLVER_SEL==j) DATA_Y1[j] <= DATA_1[DATA_W-1:DATA_W/2];
-    end
-   end
+   knn #(.HW_K(HW_K),.N_SOLVERS(N_SOLVERS),.DATA_W(DATA_W)) knn0
+     (
+      .DATA_1(DATA_1),//`INPUT(DATA_1,W),
+      .DATA_2(DATA_2),//`INPUT(DATA_2,W),
+      .SOLVER_SEL(SOLVER_SEL),
+      .rst(rst_int),
+      .clk(clk),
+      .valid(valid),
+      .DONE(DONE),
+      .SEL(SEL),
+      .DATA_OUT(DATA_OUT)
+      );
 
 
-   `SIGNAL(DATA_X2, 16)
-   `SIGNAL(DATA_Y2, 16)
-  
-    reg [DATA_W/4-1:0] data_out_solvers [N_SOLVERS-1:0];
-    `SIGNAL(data_out_int, 8)
-
-
-   `COMB begin
-
-    /*
-    DATA_Y1=DATA_1[31:16];
-    DATA_X1=DATA_1[15:0];
-    */
-
-    DATA_Y2=DATA_2[31:16];
-    DATA_X2=DATA_2[15:0];
-   end
 
 
    //
    //BLOCK 64-bit time counter & Free-running 64-bit counter with enable and soft reset capabilities
    //
-/*   
-   pipeline_sorter #(.HW_K(HW_K)) pipeline_sorter0 
+/*
+   pipeline_sorter #(.HW_K(HW_K)) pipeline_sorter0
    (
      .rst(rst_int),
      .clk(clk),
@@ -85,31 +61,7 @@ module iob_knn
     .DATA_Y1(DATA_Y1),
     .DATA_Y2(DATA_Y2)
    );
-*/   
-
-  genvar i;
-  generate 
-    for(i = 0; i < N_SOLVERS; i=i+1) begin
-      pipeline_sorter #(.HW_K(HW_K)) pipeline_sorter0
-      (
-        .rst(rst_int),
-        .clk(clk),
-        .valid(valid),
-        .DONE(DONE),
-        .SEL(SEL),
-        .DATA_OUT(data_out_solvers[i]),
-        .DATA_X1(DATA_X1[i]),
-        .DATA_X2(DATA_X2),
-        .DATA_Y1(DATA_Y1[i]),
-        .DATA_Y2(DATA_Y2)
-      );
-    end
-  endgenerate
-
-
-   `COMB data_out_int=data_out_solvers[SOLVER_SEL];
-    `SIGNAL2OUT(DATA_OUT, data_out_int)
-
+*/
    //ready signal
    `SIGNAL(ready_int, 1)
    `REG_AR(clk, rst, 0, ready_int, valid)
---
+++ hardware/src/modulo.v
@@ -0,0 +1,85 @@
+`timescale 1ns/1ps
+`include "iob_lib.vh"
+
+module knn
+  #(
+    parameter W=32,
+    parameter HW_K=10,
+    parameter N_SOLVERS = `N_SOLVERS,
+    parameter DATA_W = `DATA_W //NODOC Data word width
+  )
+    (
+     input signed [W-1:0] DATA_1,//`INPUT(DATA_1,W),
+     input signed [W-1:0] DATA_2,//`INPUT(DATA_2,W),
+     `INPUT(SOLVER_SEL,W/2),
+     `INPUT(rst,1),
+     `INPUT(clk,1),
+     `INPUT(valid,1),
+     `INPUT(DONE, 1),
+     input [15:0] SEL,
+     output [W/4-1:0] DATA_OUT
+     );
+
+
+    //`SIGNAL(DATA_X1, 16)
+    //`SIGNAL(DATA_Y1, 16)
+    `SIGNAL(DATA_X2, 16)
+    `SIGNAL(DATA_Y2, 16)
+
+     reg [DATA_W/4-1:0] data_out_solvers [N_SOLVERS-1:0];
+     `SIGNAL(data_out_int, 8)
+
+
+     reg signed [DATA_W/2-1:0] DATA_X1 [N_SOLVERS-1:0];
+     reg signed [DATA_W/2-1:0] DATA_Y1 [N_SOLVERS-1:0];
+
+     integer j;
+
+     always @(posedge clk, posedge rst) begin
+       for(j=0; j < N_SOLVERS; j=j+1) begin
+        if (rst) DATA_X1[j] <= 0; else if (SOLVER_SEL==j) DATA_X1[j] <= DATA_1[DATA_W/2-1:0];
+      end
+     end
+
+     always @(posedge clk, posedge rst) begin
+       for(j=0; j < N_SOLVERS; j=j+1) begin
+        if (rst) DATA_Y1[j] <= 0; else if (SOLVER_SEL==j) DATA_Y1[j] <= DATA_1[DATA_W-1:DATA_W/2];
+      end
+     end
+
+
+   `COMB begin
+     /*
+     DATA_Y1=DATA_1[31:16];
+     DATA_X1=DATA_1[15:0];
+     */
+
+     DATA_Y2=DATA_2[31:16];
+     DATA_X2=DATA_2[15:0];
+    end
+
+
+    genvar i;
+    generate
+      for(i = 0; i < N_SOLVERS; i=i+1) begin
+        pipeline_sorter #(.HW_K(HW_K)) pipeline_sorter0
+        (
+          .rst(rst),
+          .clk(clk),
+          .valid(valid),
+          .DONE(DONE),
+          .SEL(SEL),
+          .DATA_OUT(data_out_solvers[i]),
+          .DATA_X1(DATA_X1[i]),
+          .DATA_X2(DATA_X2),
+          .DATA_Y1(DATA_Y1[i]),
+          .DATA_Y2(DATA_Y2)
+        );
+      end
+    endgenerate
+
+    `COMB data_out_int=data_out_solvers[SOLVER_SEL];
+    `SIGNAL2OUT(DATA_OUT, data_out_int)
+
+
+endmodule
